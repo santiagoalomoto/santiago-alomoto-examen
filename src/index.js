0f38466443d01c23a0c678ac6f268738b7d50dd4
santiagoalomoto <sic.alomoto@yavirac.edu.ec>
2025-11-27 15:45:08 -0500

    cambio10

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.get("/", (req, res) => {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>santiagoalomoto verificando los cambios</title>
+    <title>pepito</title>
     <style>
         * {
             margin: 0;
